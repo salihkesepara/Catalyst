80cad274f25761f7914b3f8ea449efc0c0b436b2
nazirdogan <nazrdogan@gmail.com>
2015-05-12 15:56:36 +0300

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,19 @@
-angular.module('app', ['service.injects'])
+angular.module('app', ['service.injects','ngCordova'])
 
-.run(function ($ionicPlatform, db) {
+.run(function ($ionicPlatform, db,$ionicViewService,$ionicHistory,$state) {
+      $ionicPlatform.registerBackButtonAction(function () {
+
+
+        if($ionicHistory.currentStateName()==="login"){
+            navigator.app.exitApp();
+        }
+
+          $ionicViewService.nextViewOptions({
+              disableBack: true
+          });
+          $state.go('login');
+
+      }, 100);
   $ionicPlatform.ready(function () {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -24,5 +37,5 @@ angular.module('app', ['service.injects'])
   }
   $urlRouterProvider.otherwise("/login");
 }).config(['$httpProvider', function($httpProvider) {
-     // $httpProvider.defaults.timeout = 5000;
+      $httpProvider.defaults.timeout = 5000;
     }]);
