925e8c443ac949383ca976bd2828872f96af3187
Salih Kesepara <salih.kesepara@etiya.com>
2015-05-13 13:59:08 +0300

    Statusbar plugin i eklendi

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,31 @@
-angular.module('app', ['service.injects','ngCordova'])
+angular.module('app', ['service.injects', 'ngCordova'])
 
-.run(function ($ionicPlatform, db,$ionicHistory,$state,$cordovaDialogs) {
-      $ionicPlatform.registerBackButtonAction(function () {
-          $ionicHistory.nextViewOptions({
-              disableBack: true
-          });
+.run(function ($ionicPlatform, db, $ionicHistory, $state, $cordovaDialogs, $cordovaStatusbar) {
+  $ionicPlatform.registerBackButtonAction(function () {
+    $ionicHistory.nextViewOptions({
+      disableBack: true
+    });
 
-        if($ionicHistory.currentStateName()==="login"){
-            navigator.app.exitApp();
+    if ($ionicHistory.currentStateName() === "login") {
+      navigator.app.exitApp();
+    }
+    $cordovaDialogs.confirm('Çıkmak istediğinizden eminmisiniz?', 'Çıkış', ['Evet', 'Hayır'])
+      .then(function (buttonIndex) {
+
+        var btnIndex = buttonIndex;
+        if (btnIndex === 1) {
+
+          $state.go('login');
         }
-          $cordovaDialogs.confirm('Çıkmak istediğinizden eminmisiniz?', 'Çıkış', ['Evet','Hayır'])
-              .then(function(buttonIndex) {
-
-                  var btnIndex = buttonIndex;
-                  if(btnIndex===1){
-
-                      $state.go('login');
-                  }
-              });
-
-
-
-      }, 100);
+      });
+  }, 100);
   $ionicPlatform.ready(function () {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
       StatusBar.styleLightContent();
+      $cordovaStatusbar.overlaysWebView(true);
     }
 
     db.init().then(function (result) {
@@ -38,7 +36,7 @@ angular.module('app', ['service.injects','ngCordova'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
   $ionicConfigProvider.backButton.text('').previousTitleText(false);
   if (!ionic.Platform.isIOS()) {
     $ionicConfigProvider.scrolling.jsScrolling(false);
