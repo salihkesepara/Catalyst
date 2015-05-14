795d1a7ccf693d5c6bb0ac8da1969ef9bae18fec
nazirdogan <nazrdogan@gmail.com>
2015-05-14 12:00:57 +0300

    no message

--- www/js/services/overview.js
+++ www/js/services/overview.js
@@ -8,7 +8,7 @@ angular.module('service.overview', [])
         plotBorderWidth: 0,
         plotShadow: false,
         height: $(window).width() > 500 ? 600 : 350,
-        marginTop: $(window).width() > 500 ? -100 : -200,
+        marginTop: $(window).width() > 500 ? -100 : -200
       },
       colors: ['#f3742a', '#387ef5', '#8bbc21'],
       title: {
